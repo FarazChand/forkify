f12ada0b2ea92b9dae0e4998351d3a5aceb69456
faraz.chand@hotmail.com <faraz.chand@hotmail.com>
2022-04-18 08:05:52 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bd70986a-5cbb-4ceb-8c1d-8ba6dcb74c45';
+export const MODAL_CLOSE_SEC = 2.5;
