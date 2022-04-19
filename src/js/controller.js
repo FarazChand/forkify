c46758038c29fb9b91a7424e5f80bb9dbbd68215
faraz.chand@hotmail.com <faraz.chand@hotmail.com>
2022-04-18 21:27:13 -0400

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -132,10 +132,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -144,7 +140,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
 // window.addEventListener('hashchange', showRecipe);
